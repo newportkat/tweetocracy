c8f5ac2342248aa2718172202b8ec5c01faf0b5d
newportkat <thomas.katilius@gmail.com>
2023-04-06 14:29:27 +1000

    updated party

--- client/src/pages/Party.tsx
+++ client/src/pages/Party.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import { alpData } from "../data/alpData"
+import { coalitionData } from "../data/coalitionData"
 
 const Party = () => {
-  return <div>Party</div>;
-};
+    const { id } = useParams()
+    const [politicians, setPoliticians] = useState([])
 
-export default Party;
+    useEffect(() => {
+        if (id === "alp") {
+            setPoliticians(alpData)
+        } else if (id === "coalition") {
+            setPoliticians(coalitionData)
+        }
+    }, [id])
+
+    return (
+        <main>
+            <h2>List of {id} Ministers:</h2>
+            <ul>
+                {politicians.map((politician) => (
+                    <li key={politician.id}>
+                        <Link to={`/politicians/${politician.id}`}>
+                            {politician.latest_member.name.first}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+}
+
+export default Party
