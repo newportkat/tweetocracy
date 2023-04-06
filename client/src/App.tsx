813de2703badc684eea9ab1ec0de0d8ec5d5bd61
newportkat <thomas.katilius@gmail.com>
2023-04-06 14:28:12 +1000

    setup react-router-dom structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React from "react"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Error from "./pages/Error"
+import Home from "./pages/Home"
+import Parties from "./pages/Parties"
+import Party from "./pages/Party"
+import Policies from "./pages/Policies"
+import Policy from "./pages/Policy"
+import Politician from "./pages/Politician"
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/parties",
+                element: <Parties />,
+            },
+            {
+                path: "/parties/:id",
+                element: <Party />,
+            },
+            {
+                path: "/politicians/:id",
+                element: <Politician />,
+            },
+            {
+                path: "/policies",
+                element: <Policies />,
+            },
+            {
+                path: "/policies/:id",
+                element: <Policy />,
+            },
+        ],
+    },
+])
 
 const App = () => {
-  return <div>App</div>;
-};
+    return (
+        <div>
+            <RouterProvider router={router} />
+        </div>
+    )
+}
 
-export default App;
+export default App
