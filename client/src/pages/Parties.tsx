2d1e52486a85da440f40fe3fe95dbeb9d387e20b
newportkat <thomas.katilius@gmail.com>
2023-04-06 14:29:05 +1000

    updated parties

--- client/src/pages/Parties.tsx
+++ client/src/pages/Parties.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from "react"
+import { Link } from "react-router-dom"
 
 const Parties = () => {
-  return <div>Parties</div>;
-};
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/parties/alp">Labor Party</Link>
+                </li>
+                <li>
+                    <Link to="/parties/coalition">Coalition</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
 
-export default Parties;
+export default Parties
