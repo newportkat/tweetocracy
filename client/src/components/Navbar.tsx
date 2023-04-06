a8eec316b4aa10a62f5b2a431e4e474f31ad7747
newportkat <thomas.katilius@gmail.com>
2023-04-06 14:28:40 +1000

    updated navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,22 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
-    return <div>Navbar</div>
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Tweetocracy</Link>
+                </li>
+                <li>
+                    <Link to="/parties">Parties</Link>
+                </li>
+                <li>
+                    <Link to="/policies">Policies</Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
