efddc9bdbb71831d893f4b93b29be12ab8409273
newportkat <thomas.katilius@gmail.com>
2023-04-06 14:32:05 +1000

    updated policy page to fetch relevant data

--- client/src/pages/Policy.tsx
+++ client/src/pages/Policy.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 const Policy = () => {
-  return <div>Policy</div>;
-};
+    const { id } = useParams()
+    const [policy, setPolicy] = useState(null)
 
-export default Policy;
+    const fetchPolicy = async () => {
+        try {
+            const response = await axios.get(
+                `https://theyvoteforyou.org.au/api/v1/policies/${id}.json?key=${
+                    import.meta.env.VITE_THEY_VOTE_FOR_YOU_API_KEY
+                }`
+            )
+            setPolicy(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchPolicy()
+    }, [])
+
+    return (
+        <div>
+            {policy ? (
+                <div>
+                    <h2>{policy.name}</h2>
+                    <p>{policy.description}</p>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    )
+}
+
+export default Policy
