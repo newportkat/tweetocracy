88372e629218969cca9130174a73582ddfd0e613
newportkat <thomas.katilius@gmail.com>
2023-04-09 18:11:21 +1000

    added loading spinner

--- client/src/components/LatestTweets.tsx
+++ client/src/components/LatestTweets.tsx
@@ -57,7 +57,10 @@ const LatestTweets = () => {
                     ))}
                 </div>
             ) : (
-                <p className="p-6">Loading tweets...</p>
+                    <div
+                        className="m-6 h-10 w-10 animate-spin rounded-full
+                    border-8 border-solid border-gray-800 border-t-transparent"
+                    ></div>
             )}
             {tweets.length > 0 && (
                 <button
