d8637526c8d34b79d158521c999216670cd44c7f
newportkat <thomas.katilius@gmail.com>
2023-04-10 17:56:59 +1000

    party styling finished

--- client/src/components/PoliticianCard.tsx
+++ client/src/components/PoliticianCard.tsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const PoliticianCard = ({ name, electorate }) => {
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>Electorate: {electorate}</p>
+        <div className="flex w-72 flex-col gap-2 rounded bg-white p-4 shadow-xl">
+            <h3 className="font-semibold">{name}</h3>
+            <p className="text-xs">Electorate: {electorate}</p>
         </div>
     )
 }
