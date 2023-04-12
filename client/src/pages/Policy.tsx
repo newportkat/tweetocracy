1302d8b493c6519e6a5eb2561615a922008c3806
newportkat <thomas.katilius@gmail.com>
2023-04-12 11:36:01 +1000

    policy page styling completed

--- client/src/pages/Policy.tsx
+++ client/src/pages/Policy.tsx
@@ -1,7 +1,8 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import Voter from "../components/Voter"
+import { Link, useParams } from "react-router-dom"
+import Loader from "../components/Loader"
+import VoterCard from "../components/VoterCard"
 
 const Policy = () => {
     const { id } = useParams()
@@ -30,7 +31,13 @@ const Policy = () => {
                 latestDate = divisionDate
             }
         })
-        return latestDate.toISOString().split("T")[0]
+        return latestDate
+            .toLocaleDateString("en-AU", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+            })
+            .replace(/\//g, "-") // replace slashes with dashes
     }
 
     const handleFilterChange = (e) => {
@@ -52,33 +59,64 @@ const Policy = () => {
     return (
         <div>
             {policy ? (
-                <div>
-                    <h2>{policy.name}</h2>
-                    <p>
-                        (Latest Vote:{" "}
-                        {getMostRecentVoteDate(policy.policy_divisions)})
-                    </p>
-
-                    <p>{policy.description}</p>
-                    <div>
-                        <label htmlFor="filter">Filter voters:</label>
-                        <select id="filter" onChange={handleFilterChange}>
-                            <option value="all">All</option>
-                            <option value="yes">Voted Yes</option>
-                            <option value="no">Voted No</option>
-                        </select>
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-col items-center justify-center gap-1 bg-gray-800 p-6 text-center font-extrabold uppercase tracking-widest text-white">
+                        <h1>{policy.name}</h1>
+                        <p className="text-xs font-medium">
+                            LATEST VOTE:{" "}
+                            {getMostRecentVoteDate(policy.policy_divisions)}
+                        </p>
                     </div>
-                    {filteredVoters.map((voter) => (
-                        <Voter
-                            key={voter.person.id}
-                            name={`${voter.person.latest_member.name.first} ${voter.person.latest_member.name.last}`}
-                            party={voter.person.latest_member.party}
-                            vote={voter.voted}
-                        />
-                    ))}
+                    <div className="flex flex-col items-center p-4">
+                        <Link
+                            to="/policies"
+                            className="rounded border-2 border-white bg-gray-800 px-4 py-2 text-xs font-bold tracking-wider text-white"
+                        >
+                            BACK TO POLICIES
+                        </Link>
+                        <p className="p-4 leading-relaxed">
+                            {policy.description.charAt(0).toUpperCase() +
+                                policy.description.slice(1)}
+                            .
+                        </p>
+                        <div className="flex items-center justify-center gap-2">
+                            <label
+                                htmlFor="filter"
+                                className="text-sm font-medium"
+                            >
+                                FILTER:
+                            </label>
+                            <select
+                                id="filter"
+                                onChange={handleFilterChange}
+                                className="bg-white p-2 shadow-xl"
+                            >
+                                <option value="all">All</option>
+                                <option value="yes">Voted Yes</option>
+                                <option value="no">Voted No</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap items-center justify-center gap-4 p-4 mb-4 lg:px-8">
+                        {filteredVoters.map((voter) => (
+                            <VoterCard
+                                key={voter.person.id}
+                                name={`${voter.person.latest_member.name.first} ${voter.person.latest_member.name.last}`}
+                                party={voter.person.latest_member.party}
+                                vote={voter.voted}
+                            />
+                        ))}
+                    </div>
                 </div>
             ) : (
-                <p>Loading...</p>
+                <div>
+                    <div className="w-full bg-gray-800 p-6 text-center font-extrabold tracking-widest text-white">
+                        <p>LOADING POLICY...</p>
+                    </div>
+                    <div className="flex items-center justify-center p-6">
+                        <Loader />
+                    </div>
+                </div>
             )}
         </div>
     )
